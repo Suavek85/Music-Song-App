1ec1bd05163ddbe80eb0eee8d56fb6ef98600361
SNiemiec <slawekniemiec@gmail.com>
2019-04-08 20:37:32 +0200

    Updates

---
+++ precache-manifest.822fb4f36cbba50a67f4fe49e664645d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "1c419989fca3f7513882a00887881928",
+    "url": "/Suavek85/Music-Song-App/static/media/music-black-white.1c419989.jpg"
+  },
+  {
+    "revision": "c37b2f0a87442d3ca280",
+    "url": "/Suavek85/Music-Song-App/static/js/runtime~main.31120195.js"
+  },
+  {
+    "revision": "4b55868719c9fb8e82e0",
+    "url": "/Suavek85/Music-Song-App/static/js/main.2bdbc14a.chunk.js"
+  },
+  {
+    "revision": "d928e00c5d9a690d7936",
+    "url": "/Suavek85/Music-Song-App/static/js/2.6452f0e0.chunk.js"
+  },
+  {
+    "revision": "4b55868719c9fb8e82e0",
+    "url": "/Suavek85/Music-Song-App/static/css/main.cfccd609.chunk.css"
+  },
+  {
+    "revision": "7a2e6a0be039b54c4717d3524febff92",
+    "url": "/Suavek85/Music-Song-App/index.html"
+  }
+];
