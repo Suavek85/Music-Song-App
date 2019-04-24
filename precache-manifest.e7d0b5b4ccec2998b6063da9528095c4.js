ee7e4e8ecf73c958d8415e6a3197a456775b59d1
SNiemiec <slawekniemiec@gmail.com>
2019-04-24 23:53:49 +0200

    Updates

---
+++ precache-manifest.e7d0b5b4ccec2998b6063da9528095c4.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = [
+  {
+    "revision": "87e4a6a5322131eb472276b69be0093a",
+    "url": "/Music-Song-App/static/media/usa.87e4a6a5.jpg"
+  },
+  {
+    "revision": "a284017ceadb97386531040651ed6289",
+    "url": "/Music-Song-App/static/media/spain.a284017c.jpg"
+  },
+  {
+    "revision": "1c419989fca3f7513882a00887881928",
+    "url": "/Music-Song-App/static/media/music-black-white.1c419989.jpg"
+  },
+  {
+    "revision": "7223fc142c48ecbdfd7c01650d382fae",
+    "url": "/Music-Song-App/static/media/music-black-white-rotate.7223fc14.jpg"
+  },
+  {
+    "revision": "2d2c23edfec4cc77a30f08f1030a7925",
+    "url": "/Music-Song-App/static/media/brazil.2d2c23ed.jpg"
+  },
+  {
+    "revision": "9860a48a7257f7347d58",
+    "url": "/Music-Song-App/static/js/runtime~main.7067adf5.js"
+  },
+  {
+    "revision": "b7c8d8a1da1afa316d14",
+    "url": "/Music-Song-App/static/js/main.b9bd5b0a.chunk.js"
+  },
+  {
+    "revision": "121bfa9d64e66e350455",
+    "url": "/Music-Song-App/static/js/2.61cb3251.chunk.js"
+  },
+  {
+    "revision": "b7c8d8a1da1afa316d14",
+    "url": "/Music-Song-App/static/css/main.17e89baf.chunk.css"
+  },
+  {
+    "revision": "2b5966e254932c70a6ef4899f66bbccc",
+    "url": "/Music-Song-App/index.html"
+  }
+];
