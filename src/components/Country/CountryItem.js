59b4b751068e86da46ca162df412a31b05379156
SNiemiec <slawekniemiec@gmail.com>
2019-05-01 12:41:57 +0200

    CountryItem component refactored

--- src/components/Country/CountryItem.js
+++ src/components/Country/CountryItem.js
@@ -44,108 +44,44 @@ const CountryItem = props => {
           </div>
           {props.showSongs ? (
             <div>
-              <div className="mb4 ml5 tl flex items-center ">
-                <span style={props.circleStyle}>1</span>
-                <span className="white pl3 pr1 washed-red">
-                  {props.topSongs[0].track}
-                </span>
-                <span className="white pr1"> by </span>
-                <span className="white lightest-blue pr2">
-                  {props.topSongs[0].artist}
-                </span>
-                <span
-                  className="fav-wrapper grow-hov dib"
-                  onClick={props.onCountryFavClick}
-                  data-no={props.topSongs[0].number}
-                  data-id={props.topSongs[0].id}
-                >
-                  {props.topSongs[0].favClicked ? (
-                    <RedFavoriteFull />
-                  ) : (
-                    <RedFavoriteEmpty />
-                  )}
-                </span>
-                <a
-                  href={youTubeUrl(
-                    props.topSongs[0].track,
-                    props.topSongs[0].artist
-                  )}
-                  target="_blank"
-                  rel="noopener noreferrer"
-                  style={{ marginLeft: "5px" }}
-                  className="fav-wrapper grow-hov"
-                >
-                  <YoutubeIconWhite />
-                </a>
-              </div>
-              <div className="mb4 ml5 tl flex items-center ">
-                <span style={props.circleStyle}>1</span>
-                <span className="white pl3 pr1 washed-red">
-                  {props.topSongs[1].track}
-                </span>
-                <span className="white pr1"> by </span>
-                <span className="white lightest-blue pr2">
-                  {props.topSongs[1].artist}
-                </span>
-                <span
-                  className="fav-wrapper grow-hov dib"
-                  onClick={props.onCountryFavClick}
-                  data-no={props.topSongs[1].number}
-                  data-id={props.topSongs[1].id}
-                >
-                  {props.topSongs[1].favClicked ? (
-                    <RedFavoriteFull />
-                  ) : (
-                    <RedFavoriteEmpty />
-                  )}
-                </span>
-                <a
-                  href={youTubeUrl(
-                    props.topSongs[1].track,
-                    props.topSongs[1].artist
-                  )}
-                  target="_blank"
-                  rel="noopener noreferrer"
-                  style={{ marginLeft: "5px" }}
-                  className="fav-wrapper grow-hov"
-                >
-                  <YoutubeIconWhite />
-                </a>
-              </div>
-              <div className="mb4 ml5 tl flex items-center ">
-                <span style={props.circleStyle}>1</span>
-                <span className="white pl3 pr1 washed-red">
-                  {props.topSongs[2].track}
-                </span>
-                <span className="white pr1"> by </span>
-                <span className="white lightest-blue pr2">
-                  {props.topSongs[2].artist}
-                </span>
-                <span
-                  className="fav-wrapper grow-hov dib"
-                  onClick={props.onCountryFavClick}
-                  data-no={props.topSongs[2].number}
-                  data-id={props.topSongs[2].id}
-                >
-                  {props.topSongs[2].favClicked ? (
-                    <RedFavoriteFull />
-                  ) : (
-                    <RedFavoriteEmpty />
-                  )}
-                </span>
-                <a
-                  href={youTubeUrl(
-                    props.topSongs[2].track,
-                    props.topSongs[2].artist
-                  )}
-                  target="_blank"
-                  rel="noopener noreferrer"
-                  style={{ marginLeft: "5px" }}
-                  className="fav-wrapper grow-hov"
-                >
-                  <YoutubeIconWhite />
-                </a>
-              </div>
+              {[0, 1, 2].map(el => {
+                return (
+                  <div className="mb4 ml5 tl flex items-center ">
+                    <span style={props.circleStyle}>{el + 1}</span>
+                    <span className="white pl3 pr1 washed-red">
+                      {props.topSongs[el].track}
+                    </span>
+                    <span className="white pr1"> by </span>
+                    <span className="white lightest-blue pr2">
+                      {props.topSongs[el].artist}
+                    </span>
+                    <span
+                      className="fav-wrapper grow-hov dib"
+                      onClick={props.onCountryFavClick}
+                      data-no={props.topSongs[el].number}
+                      data-id={props.topSongs[el].id}
+                    >
+                      {props.topSongs[el].favClicked ? (
+                        <RedFavoriteFull />
+                      ) : (
+                        <RedFavoriteEmpty />
+                      )}
+                    </span>
+                    <a
+                      href={youTubeUrl(
+                        props.topSongs[el].track,
+                        props.topSongs[el].artist
+                      )}
+                      target="_blank"
+                      rel="noopener noreferrer"
+                      style={{ marginLeft: "5px" }}
+                      className="fav-wrapper grow-hov"
+                    >
+                      <YoutubeIconWhite />
+                    </a>
+                  </div>
+                );
+              })}
             </div>
           ) : null}
         </div>
