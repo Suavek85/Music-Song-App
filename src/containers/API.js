cf83af885d7f4d66350c8c203c5d8ce5688435b8
SNiemiec <slawekniemiec@gmail.com>
2019-05-04 21:00:28 +0200

    Fixing api https problem

--- src/containers/API.js
+++ src/containers/API.js
@@ -5,7 +5,7 @@ export const specificUrl = input => {
 };
 
 export const specificCountryUrl = code => {
-  return `https://cors-anywhere.herokuapp.com/https://api.musixmatch.com/ws/1.1/chart.tracks.get?chart_name=top&page=1&page_size=5&country=${code}&f_has_lyrics=1%20&%20apikey=${apiKey}`;
+  return `https://cors-anywhere.herokuapp.com/http://api.musixmatch.com/ws/1.1/chart.tracks.get?chart_name=top&page=1&page_size=5&country=${code}&f_has_lyrics=1%20&%20apikey=${apiKey}`;
 }
 
 export const youTubeUrl = (song, artist) => {
